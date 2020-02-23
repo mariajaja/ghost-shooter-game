11dbfb7bdcee6599468c64320267faddbf0ae3e7
Mariah Hall <hallmar94@gmail.com>
2020-02-22 20:12:15 -0500

    added main character to the screen

--- game.js
+++ game.js
@@ -4,13 +4,12 @@ var canvasBg = document.getElementById("canvasBg"),
     ctxEntities = canvasEntities.getContext("2d"),
     canvasWidth = canvasBg.width,
     canvasHeight = canvasBg.height,
-    //player1 = new Player(),
+    player1 = new Player(),
     //enemies = [],
     //numEnemies = 5,
     //obstacles = [],
     isPlaying = false,
     requestAnimFrame = window.requestAnimationFram ||
-                       window.webkitRequestAnimationFrame ||
                        window.mozRequestAnimationFrame ||
                        window.oRequestAnimationFrame ||
                        window.msRequestAnimationFrame ||
@@ -32,4 +31,73 @@ function init() {
 
 function begin() {
     ctxBg.drawImage(imgSprite, 0, 0, canvasWidth, canvasHeight, 0, 0, canvasWidth, canvasHeight);
+    isPlaying = true;
+    requestAnimFrame(loop);
 }
+
+function update() {
+    clearCtx(ctxEntities);
+    //updateAllEnemies();
+    player1.update();
+}
+
+function draw() {
+    //drawAllEnemies();
+    player1.draw();
+}
+
+function loop() {
+    if(isPlaying) {
+        update();
+        draw();
+        requestAnimFrame(loop);
+    }
+}
+
+function clearCtx(ctx) {
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+}
+
+function randomRange(min, max) {
+    return Math.floor(Math.random() * (max + 1 - min)) + min;
+}
+
+function Player() {
+    this.srcX = 0;
+    this.srcY = 600;
+    this.width = 35;
+    this.height = 54;
+    this.drawX = 400;
+    this.drawY = 300;
+    this.centerX = this.drawX + (this.width / 2);
+    this.centerY = this.drawY + (this.height / 2);
+    this.speed = 2;
+    //this.isUpKey = false;
+    //this.isRightKey = false;
+    //this.isDownKey = false;
+    //this.isLeftKey = false;
+    //this.isSpaceBar = false;
+    //this.isShooting = false;
+    //var numBullets = 10;
+    //this.bullets = [];
+    //this.currentBullet = 0;
+    // for (var i = 0; i < numBullets; i++) {
+    //     this.bullets[this.bullets.length] = new Bullet();
+    // }
+
+Player.prototype.update = function () {
+    this.centerX = this.drawX + (this.width / 2);
+    this.centerY = this.drawY + (this.height / 2);
+    //this.checkDirection();
+    //this.checkShooting();
+    //this.updateAllBullets();
+};
+
+Player.prototype.draw = function () {
+    //this.drawAllBullets();
+    ctxEntities.drawImage(imgSprite, this.srcX, this.srcY, 
+        this.width, this.height, this.drawX, this.drawY, this.width, this.height);
+};
+
+}
+
